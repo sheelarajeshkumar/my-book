51fc8fa960e8c94ef0251df10c520de32a10e382
rksheela <rajesh.sheela@oracle.com>
2021-02-22 09:18:38 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,109 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className="bg-gradient-to-r from-gray-600 to-gray-900">
+      <div className={styles.container}>
+        <Head>
+          <title>My Book</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <nav>
+          <div className="container mx-auto px-6 py-2 flex justify-between items-center">
+            <a
+              className="font-bold text-2xl lg:text-4xl text-left text-white"
+              href="#"
+            >
+              అ స మ ర్థు డు
+            </a>
+            <div className="block lg:hidden text-white">
+              <button className="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-teal-500 appearance-none focus:outline-none">
+                <svg
+                  className="fill-current h-3 w-3"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <title>Menu</title>
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                </svg>
+              </button>
+            </div>
+           {/*  <div className="hidden lg:block text-white">
+              <ul className="inline-flex">
+                <li>
+                  <a className="px-4 font-bold" href="/">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a className="px-4 hover:text-gray-800" href="#">
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a className="px-4 hover:text-gray-800" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div> */}
+          </div>
+        </nav>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+        <main className={styles.main}>
+          <div className="flex flex-col justify-center items-center max-w-sm mx-auto my-8">
+           
+            <div className="bg-gray-300 h-64 w-96 md:w-64 rounded-lg shadow-md bg-cover bg-center">
+            <img src="/book.png"/>
+            </div>
+            
+            <div className="w-96 md:w-64 bg-white -mt-5 shadow-lg rounded-lg overflow-hidden">
+              <div className="py-2 text-center font-bold uppercase tracking-wide text-gray-800">
+                అ స మ ర్థు డు
+              </div>
+              <div className="flex items-center justify-between py-2 px-3 bg-gray-400">
+                <h1 className="text-gray-800 font-bold ">₹ 300</h1>
+                <form>
+                  <script
+                    src="https://checkout.razorpay.com/v1/payment-button.js"
+                    data-payment_button_id="pl_Ge6aoEF7spuW7B"
+                    async
+                  >
+                    {" "}
+                  </script>{" "}
+                </form>
+                {/* <button className=" bg-gray-800 text-xs text-white px-2 py-1 font-semibold rounded uppercase hover:bg-gray-700">Buy Now</button> */}
+              </div>
+            </div>
+
+          </div>
+          {/* <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1> */}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
+          {/*   <p className={styles.description}>
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+              
+        </div> */}
+        </main>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
+        <footer className={styles.footer}>
           <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+            href="https://rajeshkumarsheela.surge.sh"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-white"
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            Powered by{" "}
+            <img src="https://www.jamiesale-cartoonist.com/wp-content/uploads/cartoon-business-man-free1.png" alt="Vercel Logo" className={styles.logo} />
           </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        </footer>
+      </div>
     </div>
-  )
+  );
 }
